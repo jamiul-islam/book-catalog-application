760e26d3603f01f24eaae9b3dda46ae5e4248688
jamiul-islam <jamiulislamjami@hotmail.com>
2023-07-15 12:36:17 +0600

    initialize backend

---
+++ back-end/index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 8000;
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://lucifer1112k:0.Lucifer.0@cluster0.wohac.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const db = client.db("book-collection");
+    const bookCollection = db.collection("books");
+
+    app.get("/books", async (req, res) => {
+      const cursor = bookCollection.find({});
+      const books = await cursor.toArray();
+
+      res.send({ status: true, data: books });
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Book Catalog Api");
+});
+
+app.listen(port, () => {
+  console.log(`Book Catalog app listening on port: ${port}`);
+});
