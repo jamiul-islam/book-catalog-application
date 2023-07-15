3774e6bef0a1a6226392b993502587a0605c32ea
jamiul-islam <jamiulislamjami@hotmail.com>
2023-07-15 21:52:47 +0600

    home in center

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="flex justify-between items-center h-[calc(100vh-80px)] max-w-7xl mx-auto ">
+      <div className="flex justify-center items-center h-[calc(100vh-80px)] max-w-7xl mx-auto ">
         <div>HOME</div>
       </div>
     </>
