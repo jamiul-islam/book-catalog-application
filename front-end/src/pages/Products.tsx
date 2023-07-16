e8a85be7da78167264b7c7d63f061b805d206023
jamiul-islam <jamiulislamjami@hotmail.com>
2023-07-16 23:29:40 +0600

    search bar works

--- front-end/src/pages/Products.tsx
+++ front-end/src/pages/Products.tsx
@@ -1,36 +1,66 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import ProductCard from '@/components/ProductCard';
 import { Slider } from '@/components/ui/slider';
 import { useGetProductsQuery } from '@/redux/features/products/productApi';
 import { setYearRange } from '@/redux/features/products/productSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import { IBook } from '@/types/globalTypes';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 export default function Products() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState<IBook[]>([]);
   const { data, isLoading } = useGetProductsQuery(undefined);
-
   const { filterYear, status } = useAppSelector((state) => state.product);
   const dispatch = useAppDispatch();
+  let productsData: IBook[];
 
+  // filter data
+  useEffect(() => {
+    let filteredBooks: IBook[];
+
+    if (status) {
+      filteredBooks = data?.data.filter(
+        (item: { status: boolean; publicationDate: string }) =>
+          item.status === true &&
+          Number(item.publicationDate.slice(-4)) < filterYear
+      );
+    } else if (filterYear > 0) {
+      filteredBooks = data?.data?.filter(
+        (item: { publicationDate: string }) =>
+          Number(item.publicationDate.slice(-4)) < filterYear
+      );
+    } else {
+      filteredBooks = data?.data;
+    }
+
+    setSearchResults(filteredBooks);
+  }, [data, filterYear, status]);
+
+  // slider handler
   const handleSlider = (value: number[]) => {
     dispatch(setYearRange(value[0]));
   };
 
-  let productsData;
+  // search bar result
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+  };
 
-  if (status) {
-    productsData = data?.data?.filter(
-      (item: { status: boolean; publicationDate: string }) =>
-        item.status === true &&
-        Number(item.publicationDate.slice(-4)) < filterYear
-    );
-  } else if (filterYear > 0) {
-    productsData = data?.data?.filter(
-      (item: { publicationDate: string }) =>
-        Number(item.publicationDate.slice(-4)) < filterYear
-    );
-  } else {
-    productsData = data?.data;
-  }
+  useEffect(() => {
+    const filteredBooks = searchResults.filter((book: IBook) => {
+      const { title, author, genre } = book;
+      const lowerCaseTerm = searchTerm.toLowerCase();
+      return (
+        title.toLowerCase().includes(lowerCaseTerm) ||
+        author.toLowerCase().includes(lowerCaseTerm) ||
+        genre.toLowerCase().includes(lowerCaseTerm)
+      );
+    });
+
+    setSearchResults(filteredBooks);
+  }, [searchTerm, searchResults]);
 
   return (
     <div className="grid grid-cols-12 max-w-7xl mx-auto relative ">
@@ -49,17 +79,28 @@ export default function Products() {
           <div>Year: From 0 To {filterYear}</div>
         </div>
       </div>
-      <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          productsData?.map((product: IBook) => (
-            <ProductCard
-              key={`${product.title} + ${product.author}`}
-              product={product}
-            />
-          ))
-        )}
+      <div className="col-span-9">
+        <div>
+          <input
+            type="text"
+            placeholder="Search by title, author, or genre"
+            value={searchTerm}
+            onChange={handleSearch}
+            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="pt-4 col-span-9 grid grid-cols-3 gap-10 pb-20">
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : (
+            searchResults?.map((product: IBook) => (
+              <ProductCard
+                key={`${product.title} + ${product.author}`}
+                product={product}
+              />
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
